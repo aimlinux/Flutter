466b40934d239668aecd10a8ba66e08c3ccc7180
aimlinux <114636143+aimlinux@users.noreply.github.com>
2023-04-13 18:03:05 +0900

    Add test_04.13

---
+++ test_04.13/hello.dart
@@ -0,0 +1,14 @@
+import 'dart:core';
+
+    //コメント
+    /*複数コメント*/ 
+
+main() {
+    print('Hello World!');
+    print(1+2-3*4/5%6);
+
+    var a = 1;
+    print(a);
+    a = 2;
+    print("\$で変数の値を埋め込み、$a")
+}
